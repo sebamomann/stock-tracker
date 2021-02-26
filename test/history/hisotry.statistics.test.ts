8828f7e7ea3fdda0707b56b56e16197186c95988
sebamomann <github@sebamomann.de>
2021-02-26 20:20:16 +0100

    first testcase

---
+++ test/history/hisotry.statistics.test.ts
@@ -0,0 +1,37 @@
+import {History} from "../../src/models/History";
+import {Transaction} from "../../src/models/transaction/Transaction";
+import {Stock} from "../../src/models/Stock/Stock";
+
+describe('Calculate statistical data of History', () => {
+    beforeAll(() => {
+        const __loadTransaction = jest.spyOn(History.prototype as any, 'loadTransactions');
+        __loadTransaction.mockImplementation(() => {
+            const transactions = [];
+
+            for (let i = 0; i < 5; i++) {
+                transactions.push(jest.mock(Transaction.toString()));
+            }
+
+            return transactions;
+        });
+    });
+
+    it('Should return correct amount of stocks owned', () => {
+        // mocks
+        const getTransactionQuantityMock = jest.spyOn(Transaction.prototype as any, 'getTransactionQuantity');
+        getTransactionQuantityMock
+            .mockReturnValueOnce(3)
+            .mockReturnValueOnce(2)
+            .mockReturnValueOnce(2)
+            .mockReturnValueOnce(1)
+            .mockReturnValueOnce(2);
+
+        // setup
+        const history = new History(jest.mock(Stock.toString()) as any);
+
+        // action
+        const numberOfOwnedStocks = history.numberOfOwnedStocks();
+
+        expect(numberOfOwnedStocks).toBe(10);
+    });
+});
