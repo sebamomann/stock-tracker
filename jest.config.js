67929d7037254c5fb7bcddaefa51593e5615727a
sebamomann <github@sebamomann.de>
2021-02-26 20:22:24 +0100

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "roots": [
+        "<rootDir>"
+    ],
+    "modulePaths": [
+        "<rootDir>"
+    ],
+    "testMatch": [
+        "**/__tests__/**/*.+(ts|tsx|js)",
+        "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    ],
+    "transform": {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+}
