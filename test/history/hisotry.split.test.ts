3e7ced85dac05c12f9a377c117ca6096afef5ea3
sebamomann <github@sebamomann.de>
2021-02-28 14:16:19 +0100

    2 more stock split tests

--- test/history/hisotry.split.test.ts
+++ test/history/hisotry.split.test.ts
@@ -7,7 +7,7 @@ describe('Create Stocksplit by adapting split factor of transaction of History',
     });
 
     describe('totalWorthOfCurrentlyOwnedStocks', () => {
-        it('Should return correct worth', async () => {
+        it('Correctly save all 5 updated transactions', async () => {
             //mocking
             const d = new Date();
             d.setDate(d.getDate() - 5);
@@ -41,6 +41,75 @@ describe('Create Stocksplit by adapting split factor of transaction of History',
             expect(Database.updateTransactionSplit).toHaveBeenNthCalledWith(3, transactions[2])
             expect(Database.updateTransactionSplit).toHaveBeenNthCalledWith(4, transactions[3])
             expect(Database.updateTransactionSplit).toHaveBeenNthCalledWith(5, transactions[4])
+        });
+
+        it('Correctly save 3 of 5 transactions that were split', async () => {
+            //mocking
+            const d = new Date();
+            d.setDate(d.getDate() - 5);
+
+            const mockTransaction = {
+                date: d,
+                splitFactor: 1,
+            };
+
+            const transactions: Transaction[] = [];
+            for (let i = 0; i < 3; i++) {
+                transactions.push({...mockTransaction, id: i} as any);
+            }
+
+            const d2 = new Date();
+            d2.setDate(d2.getDate() + 5);
+
+            for (let i = 3; i < 5; i++) {
+                transactions.push({...mockTransaction, id: i, date: d2} as any);
+            }
+
+            Database.loadTransactionsOfStock = jest.fn().mockReturnValue([...transactions]);
+            Database.updateTransactionSplit = jest.fn();
+
+            // setup
+            const history = new History({} as any);
+
+            // action
+            history.stockSplit(5, new Date());
+
+            // prepare expected
+            transactions.map(mTransaction => mTransaction.splitFactor *= 5);
+
+            //assert
+            expect(Database.updateTransactionSplit).toHaveBeenCalledTimes(3);
+            expect(Database.updateTransactionSplit).toHaveBeenNthCalledWith(1, transactions[0])
+            expect(Database.updateTransactionSplit).toHaveBeenNthCalledWith(2, transactions[1])
+            expect(Database.updateTransactionSplit).toHaveBeenNthCalledWith(3, transactions[2])
+        });
+
+        it('Correctly save 0 of 5 transactions that were split', async () => {
+            //mocking
+            const d = new Date();
+            d.setDate(d.getDate() + 5);
+
+            const mockTransaction = {
+                date: d,
+                splitFactor: 1,
+            };
+
+            const transactions: Transaction[] = [];
+            for (let i = 0; i < 5; i++) {
+                transactions.push({...mockTransaction, id: i} as any);
+            }
+
+            Database.loadTransactionsOfStock = jest.fn().mockReturnValue([...transactions]);
+            Database.updateTransactionSplit = jest.fn();
+
+            // setup
+            const history = new History({} as any);
+
+            // action
+            history.stockSplit(5, new Date());
+
+            //assert
+            expect(Database.updateTransactionSplit).toHaveBeenCalledTimes(0);
         });
     });
 });
