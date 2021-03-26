5c5e511532682ebef0624a3bbf22f003e1d41a1f
sebamomann <github@sebamomann.de>
2021-03-26 13:13:43 +0100

    stock split dialog cleanup

--- src/usecases/Renderer.ts
+++ src/usecases/Renderer.ts
@@ -2,39 +2,43 @@ import {IRenderer} from "../interface/IRenderer";
 import events from "events";
 
 export class Renderer extends events.EventEmitter implements IRenderer {
-    render(): void {
+    public render(): void {
     }
 
-    reset(): void {
+    public reset(): void {
         const wrapper = document.getElementById("wrapper")!;
         wrapper.innerHTML = "";
     }
 
-    htmlSpan(classes: string, text: string): any {
+    public htmlSpan(classes: string, text: string): any {
         const elem = document.createElement('span');
+
         elem.className = classes;
         elem.innerText = text;
 
         return elem;
     }
 
-    htmlDiv(classes: string): any {
+    public htmlDiv(classes: string): any {
         const elem = document.createElement('div');
+
         elem.className = classes;
 
         return elem;
     }
 
-    htmlImg(classes: string, src: string): any {
+    public htmlImg(classes: string, src: string): any {
         const elem = document.createElement('img');
+
         elem.className = classes;
         elem.src = src;
 
         return elem;
     }
 
-    htmlLink(classes: string, text: string, url: string): any {
+    public htmlLink(classes: string, text: string, url: string): any {
         const elem = document.createElement('a');
+
         elem.className = classes;
         elem.href = url;
         elem.text = text;
--- src/usecases/history/stock-split-dialog.renderer.ts
+++ src/usecases/history/stock-split-dialog.renderer.ts
@@ -10,30 +10,91 @@ export class StockSplitDialogRenderer extends Renderer {
         this.history = history;
     }
 
-    // Override
     public render() {
-        const block = document.getElementById("stock-split-form-wrapper")!;
-
         this.reset();
 
+        const block = document.getElementById("stock-split-form-wrapper")!;
+
+        let formHTML = this.htmlForm();
+
+        block.append(formHTML);
+    }
+
+    public reset() {
+        const existingForm = document.getElementById("stock-split-form")!;
+
+        if (existingForm) {
+            existingForm.remove();
+        }
+    }
+
+    private htmlForm() {
         let formHTML = document.createElement("form");
+
         formHTML.id = "stock-split-form";
 
+        let splitFactorInput = this.htmlSplitFactorInput();
+        let dateInput = this.htmlDateInput();
+        let submit = this.htmlSubmitButton();
+        let cancelButton = this.htmlCancelButton();
+
+        formHTML.append(splitFactorInput);
+        formHTML.append(dateInput);
+        formHTML.append(submit);
+        formHTML.append(cancelButton);
+
+        return formHTML;
+    }
+
+    private htmlCancelButton() {
+        let cancelButton = document.createElement("button");
+
+        cancelButton.className = "cancel button";
+        cancelButton.innerText = "Cancel";
+
+        cancelButton.addEventListener("click", _ => {
+            this.reset();
+        });
+
+        return cancelButton;
+    }
+
+    private htmlSplitFactorInput() {
         let splitFactorInput = document.createElement("input");
+
         splitFactorInput.id = "form-split-factor"
         splitFactorInput.type = "number"
         splitFactorInput.placeholder = "Split Factor";
 
+        return splitFactorInput;
+    }
+
+    private htmlDateInput() {
         let dateInput = document.createElement("input");
+
         dateInput.id = "form-price";
         dateInput.type = "date";
         dateInput.placeholder = "Datum";
 
+        return dateInput;
+    }
+
+    private htmlSubmitButton() {
         let submit = document.createElement("button");
+
         submit.className = "stock-split-button button main-button"
         submit.type = "submit";
         submit.innerText = "submit";
-        submit.addEventListener('click', (e: Event) => {
+
+        submit.addEventListener('click',
+            this.stockSplitSubmitClickListener()
+        );
+
+        return submit;
+    }
+
+    private stockSplitSubmitClickListener() {
+        return (e: Event) => {
             e.preventDefault();
             const inputs = (document.getElementById("stock-split-form") as HTMLFormElement).elements;
 
@@ -43,29 +104,6 @@ export class StockSplitDialogRenderer extends Renderer {
             this.history.stockSplit(+splitFactor, new Date(date));
 
             this.emit("split")
-        });
-
-        let cancelButton = document.createElement("button");
-        cancelButton.className = "cancel button";
-        cancelButton.innerText = "Cancel";
-        cancelButton.addEventListener("click", _ => {
-            this.reset();
-        });
-
-        formHTML.append(splitFactorInput);
-        formHTML.append(dateInput);
-        formHTML.append(submit);
-        formHTML.append(cancelButton);
-
-        block.append(formHTML);
-    }
-
-    //override
-    reset() {
-        const existingForm = document.getElementById("stock-split-form")!;
-
-        if (existingForm) {
-            existingForm.remove();
-        }
+        };
     }
 }
